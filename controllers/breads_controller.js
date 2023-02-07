1f2b08576eb78743997212547de754924d89ed84
Ricardo Sandovale <AccSandoval13@gmail.comm>
2023-02-06 19:19:54 -0800

    edit and update

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,77 +1,151 @@
-const express = require("express");
-const breads = express.Router(); // breads passed in server.js
+const express = require('express')
+const breads = express.Router()
+const seeds = require('../seeds')
 
-const Bread = require("../models/bread.js");
 
-// INDEX PAGE
-breads.get("/", (req, res) => {
-  Bread.find().then(function (foundBreads) {
-    console.log(foundBreads);
+const Bread = require('../models/bread')
+const Baker = require('../models/baker')
 
-    res.render("index", { breads: foundBreads, title: "Index Page" });
-  });
-});
 
-// // CREATE
-breads.post("/", (req, res) => {
-  console.log(req.body);
+// INDEX
+breads.get('/', (req, res) => {
+  Bread.find({}, [], {sort: {name: 1}})
+  .populate('baker')
+  .then((foundBreads) => {    
+    res.render('index', {breads: foundBreads, title: 'Index Page'})
+  })
+})
+
+
+// CREATE
+breads.post('/', (req, res) => {
+  console.log(req.body)
   if (!req.body.image) {
-    req.body.image = undefined;
+    req.body.image = undefined
   }
-  if (req.body.hasGluten === "on") {
-    req.body.hasGluten = true;
+
+
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
   } else {
-    req.body.hasGluten = false;
+    req.body.hasGluten = false
   }
-  Bread.create(req.body);
-  res.redirect("/breads");
-});
+  Bread.create(req.body)
+  res.redirect('/breads')
+})
+
+
 
 // NEW
-breads.get("/new", (req, res) => {
-  res.render("new");
-});
+breads.get('/new', (req, res) => {
+  Baker.find().then(foundBakers => {
+    res.render('new', {
+      bakers: foundBakers
+    })
+  })
+})
 
 // EDIT
-breads.get("/:indexArray/edit", (req, res) => {
-  res.render("edit", {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray,
-  });
-});
+breads.get('/:id/edit', async (req, res) => {
+
+    try {
+      const foundBakers = await Baker.find()
+      const foundBread = await Bread.findById(req.params.id)
+
+      res.render('Edit', {
+        bread: foundBread,
+        bakers: foundBakers
+      })
+
+    } catch(error){
+      console.log(error)
+      res.render('error404')
+    }
+
+
+  
+
+  // Baker.find()
+  // .then(foundBakers => {
+  //   Bread.findById(req.params.id)
+  //   .then(foundBread => {
+  //     res.render('Edit', {
+  //       bread: foundBread,
+  //       bakers: foundBakers
+  //     })
+  //   })
+  //   .catch(error =>{
+  //     console.log(error)
+  //     res.render('error404')
+  //   } )
+  // })
+})
+
+
 
 // SHOW
-breads.get("/:id", (req, res) => {
-  Bread.findById(req.params.id)
-    .then((foundBread) => {
-      res.render("show", {
-        bread: foundBread,
-      });
+breads.get('/:id', (req, res) => {
+    Bread.findById(req.params.id)
+    .populate('baker')
+    .then(foundBread => {
+        res.render('Show', {
+          bread: foundBread,
+        })
     })
-    .catch(error => {
+    .catch(error =>{
       console.log(error)
-      res.render("error404");
-    });
-  
-});
+      res.render('error404')
+    } )
+
+})
 
 // DELETE
-breads.delete("/:indexArray", (req, res) => {
-  console.log(req, query);
-  // start (2) , delete count
-  Bread.splice(req.params.indexArray, 1);
-  res.status(302).redirect("/breads");
-}); //muteable method
+breads.delete('/:id', (req, res) => {
+ 
+  Bread.findByIdAndDelete(req.params.id).then(function(deletedBread){
+    console.log(deletedBread)
+    res.status(303).redirect('/breads')
+  })
+})
 
 // UPDATE
-breads.put("/:arrayIndex", (req, res) => {
-  if (req.body.hasGluten === "on") {
-    req.body.hasGluten = true;
+breads.put('/:id', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
   } else {
-    req.body.hasGluten = false;
+    req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body;
-  res.redirect(`/breads/${req.params.arrayIndex}`);
-});
 
-module.exports = breads; // exporting breads to server.js
+  Bread.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+  .then((updatedBread) => {
+    res.redirect(`/breads/${req.params.id}`)
+  })
+  .catch(error => {
+    res.render('GenericError', {
+      error
+    })
+  })
+})
+
+
+// SEED ROUTE
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany(seeds)
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+  
+
+module.exports = breads
